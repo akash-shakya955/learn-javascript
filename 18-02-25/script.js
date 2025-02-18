faa30a4660c629a723725b00aa10db5c1a9656f8
akash-shakya955 <mr.akashshakya@gmail.com>
2025-02-18 16:07:11 +0530

    new task added

---
+++ 18-02-25/script.js
@@ -0,0 +1,23 @@
+
+
+function printStar() {
+    let num = document.getElementById("number").value;
+    let resultBox = document.getElementById("resultbox");
+    
+    if (num === "" || num <= 0) {
+        resultBox.innerHTML = "Please enter a valid number!";
+        return;
+    }
+
+    let pattern = "";
+    for (let i = 1; i <= num; i++) {
+        pattern += "* ".repeat(i) + "\n";
+    }
+
+    resultBox.innerText = pattern;
+}
+
+function clearPattern() {
+    document.getElementById("number").value = "";
+    document.getElementById("resultbox").innerHTML = "";
+}
---
+++ game/script.js
@@ -0,0 +1,11 @@
+
+function colorx() {
+    document.getElementsByTagName("button")[0].style.backgroundColor = "red";
+}
+
+function color0() {
+    document.getElementsByTagName("button")[1].style.backgroundColor = "blue";
+}
+
+
+

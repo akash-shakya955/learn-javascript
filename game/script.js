9eb47d61a92535d19702d392dfef9671494a7059
akash-shakya955 <mr.akashshakya@gmail.com>
2025-02-19 20:58:53 +0530

    tic tac toe game css n js update

--- game/script.js
+++ game/script.js
@@ -1,11 +1,20 @@
 
-function colorx() {
+function clickx() {
     document.getElementsByTagName("button")[0].style.backgroundColor = "red";
+
+    document.getElementsByClassName("box")[0].getElementsByClassName("draw")[0].innerText = "x";  
+    
+
 }
 
-function color0() {
+function click0() {
     document.getElementsByTagName("button")[1].style.backgroundColor = "blue";
+
+    document.getElementsByClassName("box")[0].getElementsByClassName("draw")[0].innerText = "0";
+
 }
+
+
 
 
 

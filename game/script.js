b7ffc94afc96630e931c17e1e27e9a28c74faa95
akash-shakya955 <mr.akashshakya@gmail.com>
2025-02-20 00:41:24 +0530

    button enabled n disabled update

--- game/script.js
+++ game/script.js
@@ -1,20 +1,25 @@
 
 function clickx() {
-    document.getElementsByTagName("button")[0].style.backgroundColor = "red";
-
-    document.getElementsByClassName("box")[0].getElementsByClassName("draw")[0].innerText = "x";  
     
+    let index = document.getElementById("number").value;
+
+    document.getElementsByClassName("box")[index].getElementsByClassName("draw")[0].innerText = "x";  
+    
+    document.getElementById("btn1").disabled = true ;
+    document.getElementById("btn2").enabled = false;
 
 }
 
 function click0() {
-    document.getElementsByTagName("button")[1].style.backgroundColor = "blue";
+    
+    let index = document.getElementById("number").value;
 
-    document.getElementsByClassName("box")[0].getElementsByClassName("draw")[0].innerText = "0";
+    document.getElementsByClassName("box")[index].getElementsByClassName("draw")[0].innerText = "0";
+
+    document.getElementById("btn1").enabled = true;
+    document.getElementById("btn2").disabled = false;
 
 }
-
-
 
 
 
